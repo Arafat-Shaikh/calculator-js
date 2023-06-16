c0027962e5546323149fddcd4f64889be4868706
Arafat <arafatshaikh823@gmail.com>
2023-06-16 13:37:14 +0530

    files modified

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const inputBox = document.querySelector(".wrapper input");
 const nums = document.querySelectorAll(".nums");
 
+
+
+let opt = "";
 nums.forEach((btn)=>{
     btn.addEventListener("click",()=>{
         if(inputBox.value == "0"){
-            inputBox.value == ""
+            inputBox.value  = "";
         }
         let btnVal = btn.textContent;
+
+        if(btnVal == "/" || btnVal == "%" || btnVal == "*" || btnVal == "-" || btnVal == "+"){
+             opt = btnVal;
+             console.log(typeof opt);
+        }
+
+
         if(btnVal != "C" && btnVal != '←' && btnVal != "="){
-           
             inputBox.value += btn.textContent;
             
         }
@@ -17,8 +26,48 @@ nums.forEach((btn)=>{
             let inputVal = inputBox.value;
             inputVal = Math.floor(inputVal / 10)
             inputBox.value = inputVal
-            console.log(inputBox.value)
-            console.log(typeof inputBox.value)
+            
         }
+
+        if(btnVal == "="){
+            calVal();
+        }
+
+        if(btnVal == "C"){
+            inputBox.value = "";
+        }
+
+    
     })
 })
+
+function calVal(){
+    let inputVal = inputBox.value;
+    let inputArr = inputVal.split("");
+    let firstNumber = "";
+    let secondString = "";
+    let fNum = true;
+    let sNum = false;
+    let secondNumber = "";
+    let secNum = "";
+    console.log(opt);
+    for(let i=0; i<inputArr.length; i++){
+
+        if(inputArr[i] == opt){
+            fNum = false;
+            sNum = true;
+        } 
+        if(fNum){
+            firstNumber += Number(inputArr[i]);
+        }
+        if(sNum){
+            secondString += inputArr[i];
+        }
+
+         secondNumber = secondString.substring(1);
+
+          secNum = Number(secondNumber)
+    }
+    let result = eval(firstNumber + opt + secNum); 
+    console.log(result);
+}
